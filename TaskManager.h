71978b5dd7c3ee88e059cca83e2d944f79f2ab79
mguevarahernandez07-lang <mguevarahernandez07@gmail.com>
2025-12-14 23:29:38 -0800

    Update TaskManager.h

--- TaskManager.h
+++ TaskManager.h
@@ -1,1 +1,21 @@
+#ifndef TASKMANAGER_H
+#define TASKMANAGER_H
+
+#include <memory>
+#include <string>
+#include "Task.h"
+
+class TaskManager {
+private:
+    std::unique_ptr<Task[]> tasks;
+    int size;
+    int capacity;
+
+public:
+    TaskManager(int capacity);
+    void addTask(const std::string& desc);
+    void listTasks() const;
+};
+
+#endif
 
