3cda30bc8f2915bdd03a7e8ee7f1bafad314463d
mguevarahernandez07-lang <mguevarahernandez07@gmail.com>
2025-12-14 23:26:42 -0800

    Add Task class definition
    Define the Task class with attributes and methods.

--- Task.h
+++ Task.h
@@ -1,1 +1,23 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+
+class Task {
+private:
+    int id;
+    std::string description;
+    bool completed;
+
+public:
+    Task();
+    Task(int id, const std::string& desc);
+
+    void markCompleted();
+    bool isCompleted() const;
+    int getId() const;
+    std::string getDescription() const;
+};
+
+#endif
 
