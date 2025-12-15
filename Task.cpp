da7cd235d1ad02f19ec4f8901901f1fc5b872901
mguevarahernandez07-lang <mguevarahernandez07@gmail.com>
2025-12-14 23:27:10 -0800

    Implement Task class with basic functionality

--- Task.cpp
+++ Task.cpp
@@ -1,1 +1,23 @@
+#include "Task.h"
+
+Task::Task() : id(0), description(""), completed(false) {}
+
+Task::Task(int id, const std::string& desc)
+    : id(id), description(desc), completed(false) {}
+
+void Task::markCompleted() {
+    completed = true;
+}
+
+bool Task::isCompleted() const {
+    return completed;
+}
+
+int Task::getId() const {
+    return id;
+}
+
+std::string Task::getDescription() const {
+    return description;
+}
 
